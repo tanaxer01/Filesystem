b81eadeffc93a9e3adb2cefa534175bfd28ec4da
naikelin <nicolas.nunez2@mail.udp.cl>
2021-06-25 00:42:13 -0400

    fix rutas

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <sys/stat.h>
 #include <string.h>
 // Nodo de Cumulo de Unidades de Memoria
+
 typedef struct NodoCUM{
   char* name;
   char* path;
@@ -31,7 +32,7 @@ int mvWC(KK failsisten32, char* path);
 int modWC(KK failsisten32, char* path);
 
 int main(){
-	NODO head = { .name="root", .type=1, .path="./", .child = NULL, .next = NULL };
+	NODO head = { .name="root", .type=1, .path=".", .child = NULL, .next = NULL };
 	KK   table = { .root=&head, .curr=&head };
 
 	mKKdir( table, "hijo");
@@ -44,29 +45,27 @@ int main(){
 	rmKKdir( table, "hijo");
 	printf("[+] %s %s \n", table.root->name, table.root->child->name );
 
-
 }
 
 
-//  TODO ver por que pinches vergas no funcionaba de la otra forma
-//  TODO ver pq el strcat funciona bien 
 int mKKdir(KK failsisten32, char* name){
 	/*	Inserta un directorio en el nodo actual de la tabla, 
 	si ya existe un directorio lo coloca en la lista enlazada */
 
-
 	char *new_path = (char *)malloc(sizeof(char)*1024);
 	memcpy(new_path, failsisten32.curr->path, sizeof(failsisten32.curr->path));
-	strcat(new_path,"/");
+	strcat(new_path, "/");
 	strcat(new_path,name);
 
+	printf("Creando: %s\n", new_path);
 
 
 	int res = mkdir(new_path, 0777);
 	if(res < 0)
 		return -1;
 	
-	if( failsisten32.curr->child == NULL ){
+	if( failsisten32.curr->child == NULL ) {
+
 		failsisten32.curr->child = (NODO *)malloc(sizeof(NODO));
 		failsisten32.curr->child->name  = name;
 		failsisten32.curr->child->type  = 1;
@@ -74,7 +73,8 @@ int mKKdir(KK failsisten32, char* name){
 		failsisten32.curr->child->child = NULL;
 		failsisten32.curr->child->next  = NULL;
 	
-	}else{
+	} else {
+
 		NODO* temp = failsisten32.curr->child;
 		while( temp->next != NULL )
 			temp = temp->next;
@@ -85,6 +85,7 @@ int mKKdir(KK failsisten32, char* name){
 		temp->next->type   = 1;
 		temp->next->child  = NULL;
 		temp->next->next   = NULL;
+
 	}	
 
 	return 0;
@@ -98,22 +99,26 @@ int rmKKdir(KK failsisten32, char* name){
 	if( failsisten32.curr->child == NULL ){
 		// no hay dirs
 		return -1;
-	}else if( failsisten32.curr->child->name == name){
+
+	} else if( failsisten32.curr->child->name == name){
 		// es el primer dir
 		int res = rmdir( failsisten32.curr->child->path );
-		if(res < 0)
-			return -1;
+		if(res < 0) return -1;
 
 		// Manejar archivos
 		NODO* temp = failsisten32.curr->child->next;
+		printf("Removiendo: %s\n", failsisten32.curr->child->path);
 		free( failsisten32.curr->child );	
 		failsisten32.curr->child = temp;
 	}else{
 		// es otro dir
 		int res = rmdir( failsisten32.curr->child->path );
+
+		printf("Removiendo: %s\n", failsisten32.curr->child->path );
 		if(res < 0)
 			return -1;
 		// Manejar archivos
+
 		NODO* temp = failsisten32.curr->child;
 		while(temp->next != NULL){
 			if( temp->next->name == name){
